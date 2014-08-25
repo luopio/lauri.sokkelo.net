5e56253c8c1d052a2e12137ce230b4283e0aef38
Lauri Kainulainen <lauri.kainulainen@gmail.com>
2014-08-25 22:49:20 +0300

    sinatra..

--- pull_me/sinatra_stays_up_late.rb
+++ pull_me/sinatra_stays_up_late.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
+require 'date'
 
 post '/' do
-  puts "---------------- Building and pulling"
+  puts "-------------*-* Building and pulling #{DateTime.now}"
   value = `./jekyll_pull_and_build.sh`
   value
 end

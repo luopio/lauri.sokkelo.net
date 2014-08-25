eab90f940ed9f6028b261174df591922339e2728
Lauri Kainulainen <lauri.kainulainen@gmail.com>
2014-08-25 22:46:49 +0300

    key handling was a pretty different..

--- pull_me/sinatra_stays_up_late.rb
+++ pull_me/sinatra_stays_up_late.rb
@@ -1,13 +1,8 @@
 require 'sinatra'
 
 post '/' do
-  if params[:secret] == ENV['KEY']
-    puts "---------------- Building and pulling"
-    value = `./jekyll_pull_and_build.sh`
-    value
-  else
-    puts "---------------- Key missing or wrong", params.inspect
-    "Sinatra says no"
-  end
+  puts "---------------- Building and pulling"
+  value = `./jekyll_pull_and_build.sh`
+  value
 end
 

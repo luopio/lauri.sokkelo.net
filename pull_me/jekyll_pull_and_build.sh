660d1c3f8f9514766c7e71285d12a46154c219e6
Lauri Kainulainen <lauri.kainulainen@gmail.com>
2014-08-25 22:58:24 +0300

    ssshhh

--- pull_me/jekyll_pull_and_build.sh
+++ pull_me/jekyll_pull_and_build.sh
@@ -3,8 +3,7 @@
 GIT_ROOT=/var/www/lauri
 # GIT_ROOT=/Users/lauri/code/luopio.github.io/
 
-cd $GIT_ROOT &&
-git pull &&
-bundle install &&
-jekyll build
+cd $GIT_ROOT && git pull
+cd $GIT_ROOT && bundle install
+cd $GIT_ROOT && jekyll build
 

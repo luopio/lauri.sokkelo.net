d711cfa2fe095bebacd19d2764db983eb2f13128
Lauri Kainulainen <lauri.kainulainen@gmail.com>
2014-08-25 22:36:00 +0300

    trying out sinatra

--- pull_me/sinatra_stays_up_late.rb
+++ pull_me/sinatra_stays_up_late.rb
@@ -1,11 +1,13 @@
 require 'sinatra'
 
 post '/' do
-    if params[:key] == ENV['KEY']
-        value = `./jekyll_pull_and_build.sh`
-        value
-    else
-        "Sinatra says no"
-    end
+  if params[:secret] == ENV['KEY']
+    puts "---------------- Building and pulling"
+    value = `./jekyll_pull_and_build.sh`
+    value
+  else
+    puts "---------------- Key missing or wrong", params.inspect
+    "Sinatra says no"
+  end
 end
 

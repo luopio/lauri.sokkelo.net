f670bf5537489884164a5bb5ba744cb1a7b7f5a7
Lauri Kainulainen <lauri.kainulainen@gmail.com>
2016-10-19 09:11:39 +0300

    two ways to get the params

--- pull_me/sinatra_stays_up_late.rb
+++ pull_me/sinatra_stays_up_late.rb
@@ -4,18 +4,22 @@ require 'json'
 require 'pp'
 
 post '/' do
-  request.body.rewind
-  payload = JSON.parse request.body.read
-  # project = params[:project]
-  # branch = params[:branch]
-  # id = params[:id]
+  if params[:project]
+    project = params[:project]
+    branch = params[:branch]
+    id = params[:id]
+    msg = ''
+  else
+    request.body.rewind
+    payload = JSON.parse request.body.read
+    project = payload['repository']['name']
+    branch = payload['ref'].split('/').last
+    id = payload['head_commit']['id']
+    msg = payload['head_commit']['message']
+  end
   # puts "------------"
   # pp(payload)
   # puts "------------"
-  project = payload['repository']['name']
-  branch = payload['ref'].split('/').last
-  id = payload['head_commit']['id']
-  msg = payload['head_commit']['message']
   puts "-/-/-/-|-\\-\\-\\- Building #{project}/#{branch}/#{id} (#{msg}) on #{DateTime.now}"
   puts handle_projects(project, branch, id)
   puts
